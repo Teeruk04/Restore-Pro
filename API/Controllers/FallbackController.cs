d92b14be4cf6857f0e652313785fc76113d3c0a9
Teeruk <96033549+Teeruk04@users.noreply.github.com>
2022-10-08 16:37:41 +0700

    heroku 2

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -6,9 +6,9 @@ namespace API.Controllers
     [AllowAnonymous]
     public class FallbackController : BaseApiController
     {
-        // public IActionResult Index()
-        // {
-        //     return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","index.html"),"text/HTML");
-        // }
+        public IActionResult Index()
+        {
+            return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot","index.html"),"text/HTML");
+        }
     }
 }
